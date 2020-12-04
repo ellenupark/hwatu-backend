fe01991fe487a3837a7f592fbe4a93a7b5b1413e
ellenupark <ellenupark@gmail.com>
2020-12-03 20:04:55 -0500

    Added index action to controllers

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,9 @@
 class CardsController < ApplicationController
+    def index
+        cards = Card.all
+        options = {
+            include: [:player]
+        }
+        render json: CardSerializer.new(cards, options)
+    end
 end
