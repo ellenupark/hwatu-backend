47a3fdb3b9fe8c9715e15b427cb19d8a065eedbd
ellenupark <ellenupark@gmail.com>
2020-12-09 17:15:25 -0500

    Added strong params to card controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,26 +1,27 @@
 class CardsController < ApplicationController
     def index
         cards = Card.all
-        options = {
-            include: [:player]
-        }
-        render json: CardSerializer.new(cards, options)
+        render json: CardSerializer.new(cards)
     end
 
     def show
         card = Card.find_by_id(params[:id])
-        options = {
-            include: [:player]
-        }
-        render json: CardSerializer.new(card, options)
+        render json: CardSerializer.new(card)
     end
 
     def update
         card = Card.find_by_id(params[:id])
-        card.update(player_id: params[:player_id]);
-            options = {
-            include: [:player]
-        }
-        render json: CardSerializer.new(card, options)
+        if card.update(card_params);
+            render json: CardSerializer.new(card)
+        else 
+            render json: {errors: card.errors.full_messages.to_sentence}
+        end
+    end
+
+    private
+
+    # Only allow a trusted parameter "white list" through.
+    def card_params
+        params.require(:card).permit(:player_id, :matched)
     end
 end
