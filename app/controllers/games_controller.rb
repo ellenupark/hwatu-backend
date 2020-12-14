c3a09dbb59cb4917fd29729e40126bc83a287575
ellenupark <ellenupark@gmail.com>
2020-12-13 22:40:32 -0500

    Added games/history route

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -18,6 +18,6 @@ class GamesController < ApplicationController
     private
 
     def game_params
-        params.require(:game).permit(:name)
+        params.require(:game).permit(:name, :points)
     end
 end
